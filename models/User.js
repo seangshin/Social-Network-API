ccce1d96304982255ab736a537229b957b7e3f28
Sean Shin <shin.seang@gmail.com>
2023-01-12 19:02:05 -0500

    added model connection, index, and User js files

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+//Create a new instance of the Mongoose scehma to define shape of each document
+const userSchema = new mongoose.Schema({
+
+  //Add individual properties and their types
+  name: { type: String, required: true },
+  //Use built-in date method to get current date
+  lastAccessed: { type: Date, default: Date.now },
+});
+
+//Using mongoose.model() to compile a model based on the schema
+const User = mongoose.model('User', userSchema);
+
+//Error handler function for errors when trying to save a document
+const handleError = (err) => console.error(err);
+
+//create an individual document that have the properties as defined in the schema
+User.create(
+  {
+    name: "Sean Shin",
+  },
+  (err) => (err ? handleError(err) : console.log('Created new document'))
+);
+
+module.exports = User;
