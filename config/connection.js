29f47fe430b4b0ad3ed1bf40682a62cbdb1537f4
Sean Shin <shin.seang@gmail.com>
2023-01-18 16:08:13 -0500

    added seeding

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 
 //creates an instance connection to a MongoDB using local host and database collection
 mongoose.connect('mongodb://127.0.0.1:27017/social-mediaDB', { useNewUrlParser: true, useUnifiedTopology: true, })
-mongoose.set('strictQuery', false);
 
 //export connection
 module.exports = mongoose.connection;
